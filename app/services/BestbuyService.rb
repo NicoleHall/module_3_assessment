4afef5df52666607037ccd01a97ffa8998c232d1
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-08 11:22:42 -0700

    added query by service method to the Bestbuy Service.rb. hid bestbuy key in the applicaiton.yml

--- app/services/BestbuyService.rb
+++ app/services/BestbuyService.rb
@@ -3,6 +3,10 @@ class BestbuyService
 
   def initialize
     @conn = Faraday.new(:url => 'http://api.bestbuy.com')
-    @api_key =
+    @api_key = ENV['bestbuy_api_key']
+  end
 
+  def query_by_decription(query)
+    response = conn.get "/v1/products(longDescription=#{query})?show=sku,name&pageSize=15&page=5&apiKey=#{@api_key}&format=json"
+    response.body
   end
