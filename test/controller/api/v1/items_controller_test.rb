828934c21cbd04aeb92c391faaabe7960997fad4
Nicole Hall <nicolealexandrahall@gmail.com>
2016-03-08 11:06:38 -0700

    added some addit'l assertations to the destroy and create tests, tests pass

--- test/controller/api/v1/items_controller_test.rb
+++ test/controller/api/v1/items_controller_test.rb
@@ -16,17 +16,20 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
   end
 
   test '#destroy' do
+    assert_difference 'Item.count', 2 do
     create_two_items
     delete :show, format: :json, id: 1
     assert_response :no_content
+  end
   end
 
   test '#create adds a new item to database' do
     assert_difference 'Item.count', 1 do
       item = {:name => "socks", :description => "fuzzy", :image_url => "png.png"}
       post :create, format: :json, item: item
+      assert_response :created
     end
   end
- 
+
 
 end
